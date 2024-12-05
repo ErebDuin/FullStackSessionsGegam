87d2a1e46c9cfbb0ec95a48df1cc5d080170ebd3
gegam <erebduin@gmail.com>
2024-12-04 21:07:07 -0800

    Gegam's new changes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import static java.lang.System.out;
+
 public class Main {
     public static void main(String[] args) {
 
+        int num1 = 80;
+        int num2 = 40;
+        int num3 = 60;
+        if (num1 < num2) {
+            out.println("Hello World!");
+        }
+        else {
+            out.println("Not Hello");
+        }
     }
 }
