bed08e1062335a098f32af30855558b33c44eacb
gegam <erebduin@gmail.com>
2024-12-10 15:19:41 -0800

    Session 3 homework submit

--- src/Session3/StudentMenu.java
+++ src/Session3/StudentMenu.java
@@ -9,46 +9,74 @@ public class StudentMenu {
         int studentId = 0;
         String firstName = "";
         String lastName = "";
-        int studentAge  = 0;
+        byte studentAge  = 0;
 
         while (true) {
             System.out.println("======== Student Menu ========");
             System.out.println("1. Add Student Details");
             System.out.println("2. View Student Details");
-            System.out.println("3. Exit");
-            System.out.println("Enter your choice: ");
+            System.out.println("3. Reset Student Details");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
 
-            int choice = scanner.nextInt();
+            byte choice = scanner.nextByte();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Enter student ID: ");
+                    System.out.print("\nEnter student ID: ");
                     studentId = scanner.nextInt();
 
-                    System.out.println("Enter first name: ");
-                    firstName = scanner.next();
+                    System.out.print("Enter first name: ");
+                    firstName = scanner.next().trim().replaceAll("\\s", "");
+                    while (true) {
+                        if (firstName.length() < 3 || firstName.length() > 15) {
+                            System.out.println("\nFirst name should be between 3 and 15 characters\n");
+                            System.out.print("Enter first name: ");
+                            firstName = scanner.next().trim();
+                        }
+                        else
+                            break;
+                    }
 
-                    System.out.println("Enter last name: ");
-                    lastName = scanner.next();
+                    System.out.print("Enter last name: ");
+                    lastName = scanner.next().trim().replaceAll("\\s", "");
+                    while (true) {
+                        if (lastName.length() < 3 || lastName.length() > 15) {
+                            System.out.println("\nLast name should be between 3 and 15 characters\n");
+                            System.out.print("Enter last name: ");
+                            lastName = scanner.next().trim();
+                        }
+                        else
+                            break;
+                    }
 
-                    System.out.println("Enter student age: ");
-                    studentAge = scanner.nextInt();
+                    System.out.print("Enter student age: ");
+                    studentAge = scanner.nextByte();
 
                     System.out.println("Information added successfully");
+                    System.out.println();
                     break;
                 case 2:
+                    System.out.println();
                     System.out.println("Student ID: " + studentId);
                     System.out.println("First Name: " + firstName);
                     System.out.println("Last Name: " + lastName);
                     System.out.println("Student Age: " + studentAge);
+                    System.out.println();
                     break;
                 case 3:
+                    studentId = 0;
+                    firstName = "";
+                    lastName = "";
+                    studentAge = 0;
+                    System.out.println("\nStudent details successfully reset!\n");
+                    break;
+                case 4:
                     scanner.close();
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Not supported option");
-                    scanner.next();
+                    System.out.print("\nNot supported sption,\nPlease try again!\n\n");
             }
         }
     }
